9957735f9f9a56fafff547f5562a3c1af317b8eb
hvruu <122508256+hvruu@users.noreply.github.com>
2023-01-22 11:03:11 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,17 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.IOException;
 
-
-class Wagon {
+abstract class Wagon {
     //создаем переменные
     private int id;
     private int capacity;
     private String type;
     private int weight;
-    private int numberOfSeats;
-
-    public Wagon(int id, int capacity, String type, int weight, int numberOfSeats) {
+    public Wagon(int id, int capacity, String type, int weight) {
         this.id = id;
         this.capacity = capacity;
         this.type = type;
         this.weight = weight;
-        this.numberOfSeats = numberOfSeats;
     }
 
     // функции геттеры для того чтобы взять информацию
@@ -35,24 +30,70 @@ class Wagon {
     public int getWeight() {
         return weight;
     }
+    public abstract int getNumberOfSeats();
+}
 
+class CompartmentWagon extends Wagon{
+    private int numberOfSeats;
+    public CompartmentWagon(int id, int capacity, int weight, int numberOfSeats) {
+        super(id, capacity, "Compartment", weight);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
 }
 
-class Train {
-    private ArrayList<Wagon> wagons;
-
-
-    public Train() {
-        this.wagons = new ArrayList<>();
-        wagons.add(new Wagon(1, 20, "Compartment", 15000, 24));
-        wagons.add(new Wagon(2, 40, "Sitting", 20000, 40));
-        wagons.add(new Wagon(3, 50, "Reserved seat", 25000, 50));
-        wagons.add(new Wagon(4, 10, "Suite", 10000, 12));
+class SittingWagon extends Wagon{
+    private int numberOfSeats;
+    public SittingWagon(int id, int capacity, int weight, int numberOfSeats) {
+        super(id, capacity, "Sitting", weight);
+        this.numberOfSeats = numberOfSeats;
     }
 
+    @Override
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+}
+
+class ReservedSeatWagon extends Wagon{
+    private int numberOfSeats;
+    public ReservedSeatWagon(int id, int capacity, int weight, int numberOfSeats) {
+        super(id, capacity, "Reserved seat", weight);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+}
+
+class SuiteWagon extends Wagon {
+    private int numberOfSeats;
+
+    public SuiteWagon(int id, int capacity, int weight, int numberOfSeats) {
+        super(id, capacity, "Suite", weight);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+}
+class Train {
+    private ArrayList<Wagon> wagons;
+    public Train() {
+        this.wagons = new ArrayList<>();
+        wagons.add(new CompartmentWagon(1, 20, 15000, 24));
+        wagons.add(new SittingWagon(2, 40, 20000, 40));
+        wagons.add(new ReservedSeatWagon(3, 50, 25000, 50));
+        wagons.add(new SuiteWagon(4, 10, 10000, 12));
+    }
     public ArrayList<Wagon> getWagons() {
         return wagons;
     }
@@ -108,27 +149,42 @@ public class Main {
                 int wagonId = scanner.nextInt();
                 train.printWagonInfo(wagonId);
             }
-
             if (choose == 2) {
-                // add new Wagon
                 System.out.print("Enter Wagon ID: ");
-                int wagonNum = scanner.nextInt();
-                System.out.print("Enter Wagon Capacity: ");
-                int wagonCapacity = scanner.nextInt();
-                System.out.print("Enter Wagon Type: ");
-                String wagonType = scanner.nextLine();
-                System.out.print("Enter Wagon Weight: ");
-                int wagonWeight = scanner.nextInt();
-                System.out.print("Enter Wagon Number of Seats: ");
-                int wagonNumberOfSeats = scanner.nextInt();
-                train.addWagon(new Wagon(wagonNum, wagonCapacity, wagonType, wagonWeight, wagonNumberOfSeats));
+                int id = scanner.nextInt();
+                System.out.print("Enter Wagon capacity: ");
+                int capacity = scanner.nextInt();
+                System.out.print("Enter Wagon weight: ");
+                int weight = scanner.nextInt();
+                System.out.print("Enter Wagon number of seats: ");
+                int numberOfSeats = scanner.nextInt();
+                System.out.print("Enter Wagon type (Compartment, Sitting, Reserved seat,Suite): ");
+                String type = scanner.next();
+                Wagon wagon;
+                switch (type) {
+                    case "Compartment":
+                        wagon = new CompartmentWagon(id, capacity, weight, numberOfSeats);
+                        break;
+                    case "Sitting":
+                        wagon = new SittingWagon(id, capacity, weight, numberOfSeats);
+                        break;
+                    case "Reserved seat":
+                        wagon = new ReservedSeatWagon(id, capacity, weight, numberOfSeats);
+                        break;
+                    case "Suite":
+                        wagon = new SuiteWagon(id, capacity, weight, numberOfSeats);
+                        break;
+                    default:
+                        System.out.println("Invalid Wagon type. Wagon not added");
+                        continue;
+                }
+                train.addWagon(wagon);
             }
             if (choose == 3) {
-                // remove existing Wagon
                 System.out.print("Enter Wagon ID to remove: ");
-                int removeId = scanner.nextInt();
-                train.removeWagon(removeId);
+                int id = scanner.nextInt();
+                train.removeWagon(id);
             }
         }
-        }
     }
+}
